59b01f8908892ffa964094b2016f55eb8da31177
sanjoyz <sanjoyz1105@gmail.com>
2018-09-04 16:00:17 +0300

    backup

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,40 @@
-var b = document.getElementById('search-button-id');
-var fisible = false;
-b.onclick = function() {
-  if (fisible) {
-    document.getElementById('search-form-id').style.display = 'none';
-    fisible = false;
-  } else {
-    document.getElementById('search-form-id').style.display = 'block';
-    fisible = true;
-  }
+var b = document.querySelector(".hotel-button");
+var modal = document.querySelector(".search-modal");
+var form = document.querySelector("form.search");
+
+var arrival = document.querySelector("#arrival-date");
+var departure = document.querySelector("#departure-date");
+var adults = document.querySelector("#adults-amount");
+var children = document.querySelector("#children-amount");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("adults");
+} catch (err) {
+  isStorageSupport = false;
 }
+
+b.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modal.classList.toggle("modal-hide");
+  modal.classList.remove("modal-error");
+  if (storage) {
+    adults.value = storage;
+  }
+  arrival.focus();
+});
+form.addEventListener("submit", function(evt) {
+  if (!arrival.value || !departure.value) {
+    evt.preventDefault();
+    modal.classList.remove("modal-error");
+    modal.offsetWidth = modal.offsetWidth;
+    modal.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("adults", adults.value);
+      localStorage.setItem("children", children.value);
+    }
+  }
+})
